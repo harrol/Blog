bab341f77a56d6d35a5ad86a54469d9e369a8bea
Harro Lissenberg <harro.lissenberg@gmail.com>
2011-09-27 21:47:07 +0200

    Re-enabled h2

---
+++ src/main/java/com/lissenberg/blog/services/UpgradeService.java
@@ -0,0 +1,11 @@
+package com.lissenberg.blog.services;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: harro
+ * Date: 27/09/2011
+ * Time: 21:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class UpgradeService {
+}
